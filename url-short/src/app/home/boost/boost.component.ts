b9dd74f458611ec1d3da3a3b2949f1e797e99c2c
Natasha Mikaela <natasha2803mikaela@gmail.com>
2024-04-22 21:53:14 -0300

    ADD: add angular, components and  images

---
+++ url-short/src/app/home/boost/boost.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boost',
+  standalone: true,
+  imports: [],
+  templateUrl: './boost.component.html',
+  styleUrl: './boost.component.scss'
+})
+export class BoostComponent {
+
+}
